5def9a35b99631e4e74ec7fc01af9783b7b3d434
mateuszlesko <mateusz.lesko@outlook.com>
2023-01-16 17:25:42 +0100

    new features: mash kettle remote control, temperature time holding raport

--- data/mash_procedure.go
+++ data/mash_procedure.go
@@ -2,6 +2,7 @@ package data
 
 type MashProcedure struct {
 	MashId               int32   `json:"MID"`
+	RecipeId             int32   `json:"RID"`
 	ProcedureCount       int32   `json:"PC"`
 	MashTemperaturesList []int32 `json:"MTpL"`
 	MashTimeList         []int32 `json:"MTmL"`
